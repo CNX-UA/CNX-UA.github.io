d4927111161dad59e41c5bee71957d197a628055
CNX-UA <cnx.ukraine@gmail.com>
2025-11-07 23:50:46 +0200

    complete overhaul of the design and direction a lot of placeholders and WIP to be finished main changes is implementation of TailwindCSS and addition of 3 more blocks and one more tab with an accordion

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ initializeContentButtons(".droplistContentTab1 button",
         initializeDroplistGeneral(".droplistButtonTab1",".droplistContentTab1","droplist",),
         ".droplistContentTab1"));
 initializeTabsGeneral(".tabBar > div", ".tabContent", "tab");
-initializeTabsGeneral(".loginSignupTab > div",".loginSignupContent", "loginSignup");
+/* initializeTabsGeneral(".loginSignupTab > div",".loginSignupContent", "loginSignup"); commented out due to not being needed*/
 initializeAccordion(".faqAccordion > button",".faqPanel","panel");
+initializeAccordion(".workExpAccordion > button",".workExpPanel","panel")
 initializeTextareaGeneral(".feedbackTextareaTab",".feedbackTextareaCounter",250);
 initializeFormButtonGeneral(".sendFeedback");
