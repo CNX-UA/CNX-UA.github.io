9ce9837eeeb7148745a24605195a7fb743f2949e
CNX-UA <cnx.ukraine@gmail.com>
2025-11-08 15:37:21 +0200

    removed unused code.

--- src/droplist/generalContentButtons.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @param {string} buttonLocation 
- * @param {function} buttonFunction 
- */
-function initializeContentButtons(buttonLocation,buttonFunction){
-    const buttons = document.querySelectorAll(buttonLocation);
-    buttons.forEach(button => button.addEventListener("click", buttonFunction));
-};
--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM is ready!"); // For testing
   console.log("Finding buttons:", document.querySelectorAll(".skillsAccordion > button"));
-/* initializeCookiePopup(); temp disabled till fix*/
-/*initializeContentButtons(".droplistContentTab1 button", 
-    handleDroplistContent(
-        initializeDroplistGeneral(".droplistButtonTab1",".droplistContentTab1","droplist",),
-        ".droplistContentTab1")); temp disabled */
 initializeTabsGeneral(".tabBar > div", ".tabContent", "tab");
-/* initializeTabsGeneral(".loginSignupTab > div",".loginSignupContent", "loginSignup"); commented out due to not being needed*/
 initializeAccordion(".faqAccordion > button",".faqPanel","panel");
 initializeAccordion(".projectsAccordion > button",".projectsPanel","panel");
 initializeAccordion(".skillsAccordion > button",".skillsPanel","panel");
 /*initializeAccordion(".workExpAccordion > button",".workExpPanel","panel") temporarily disabled */
-/*initializeTextareaGeneral(".feedbackTextareaTab",".feedbackTextareaCounter",250); temporarily disabled*/
-/*initializeFormButtonGeneral(".sendFeedback"); */
 });
