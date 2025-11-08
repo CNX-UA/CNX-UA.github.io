f04ad9e39191c980153beeabe9bc49cea98bf76c
CNX-UA <cnx.ukraine@gmail.com>
2025-11-08 14:47:14 +0200

    changed tab layout and content, fixes needed.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM is ready!"); // For testing
+  console.log("Finding buttons:", document.querySelectorAll(".skillsAccordion > button"));
 /* initializeCookiePopup(); temp disabled till fix*/
-initializeContentButtons(".droplistContentTab1 button", 
+/*initializeContentButtons(".droplistContentTab1 button", 
     handleDroplistContent(
         initializeDroplistGeneral(".droplistButtonTab1",".droplistContentTab1","droplist",),
-        ".droplistContentTab1"));
+        ".droplistContentTab1")); temp disabled */
 initializeTabsGeneral(".tabBar > div", ".tabContent", "tab");
 /* initializeTabsGeneral(".loginSignupTab > div",".loginSignupContent", "loginSignup"); commented out due to not being needed*/
 initializeAccordion(".faqAccordion > button",".faqPanel","panel");
-initializeAccordion(".workExpAccordion > button",".workExpPanel","panel")
-initializeTextareaGeneral(".feedbackTextareaTab",".feedbackTextareaCounter",250);
-initializeFormButtonGeneral(".sendFeedback");
+initializeAccordion(".projectsAccordion > button",".projectsPanel","panel");
+initializeAccordion(".skillsAccordion > button",".skillsPanel","panel");
+/*initializeAccordion(".workExpAccordion > button",".workExpPanel","panel") temporarily disabled */
+/*initializeTextareaGeneral(".feedbackTextareaTab",".feedbackTextareaCounter",250); temporarily disabled*/
+/*initializeFormButtonGeneral(".sendFeedback"); */
+});
